291c3d26a2dddec86180a7812f07d42fbd3fdded
林啸乾 <linxiaoqian@bubi.cn>
2018-12-17 22:47:38 +0800

    add

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 const app = require('./app');
-console.log(1111);
 const server = app.listen(3000, () => {
   console.log('Server is running at http://localhost:3000');
   console.log('Press CTRL-C to stop \n');
